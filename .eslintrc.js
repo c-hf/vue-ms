e2a71725d9aa664bca68918f03a25202ce6dea46
c-hf <40515181+c-hf@users.noreply.github.com>
2018-10-15 21:46:41 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/prettier',
+		'plugin:prettier/recommended',
+	],
+	rules: {
+		'prettier/prettier': 'error',
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		// 使用单引号
+		quotes: ['error', 'single'],
+		//总是以 ; 结尾
+		semi: ['error', 'always'],
+		//new时必须加小括号
+		'new-parens': 'error',
+		//禁止重复声明变量
+		'no-redeclare': 'error',
+		//一行结束后面不要有空格
+		'no-trailing-spaces': 'warn',
+		//强制驼峰法命名
+		camelcase: 'error',
+		//必须使用全等
+		eqeqeq: 'error',
+		//缩进风格
+		indent: ['error', 'tab'],
+		//禁用var，用let和const代替
+		'no-var': 'error',
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
 };
